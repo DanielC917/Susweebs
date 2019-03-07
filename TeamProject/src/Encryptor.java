77265bbb76543e00222a412a52eb8f03eb7b3978
DanielC917 <48300741+DanielC917@users.noreply.github.com>
2019-03-07 12:40:53 -0800

    Update Encryptor.java

--- TeamProject/src/Encryptor.java
+++ TeamProject/src/Encryptor.java
@@ -3,15 +3,8 @@ public class Encryptor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int j = 2;
-		int i = 0;
-		if(i > 1) {
-			System.out.println("Hello");
-		}
-		if(i < 1) {
-			System.out.println("Hello");
-		}
 		
+		// Merge test #2
 	}
 
 }
